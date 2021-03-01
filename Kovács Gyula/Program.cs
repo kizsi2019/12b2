026c0fe728c48f309b2e169f4ac6bb8bf582b29d
Kovács Gyula Dániel <kovacsgyuladaniel13@gmail.com>
2021-03-01 13:09:10 +0100

    első érettségi feladataim

---
+++ Kovács Gyula/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeldaLinqWhere
+{
+    struct Pelda
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elemek = new List<Pelda>
+            {
+                new Pelda { X = 10, Y = 20 },
+                new Pelda { X = 11, Y = 23 },
+                new Pelda { X = 44, Y = 42 },
+                new Pelda { X = 7, Y = 1 },
+                new Pelda { X = 9, Y = 12 },
+            };
+
+            {
+                var Max = elemek.Max(i => i.X);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
